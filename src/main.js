10c75695c43d3cd253ad0e265a86d175cdccef14
wuliao <1182300246@qq.com>
2022-07-01 16:41:29 +0800

    引入echarts

--- src/main.js
+++ src/main.js
@@ -15,6 +15,9 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 
+import * as echarts from 'echarts'
+Vue.prototype.$echarts = echarts //挂载到Vue实例上面
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
@@ -24,12 +27,16 @@ import '@/permission' // permission control
  * please remove it before going online ! ! !
  */
 if (process.env.NODE_ENV === 'production') {
-  const { mockXHR } = require('../mock')
+  const {
+    mockXHR
+  } = require('../mock')
   mockXHR()
 }
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, {
+  locale
+})
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
 
